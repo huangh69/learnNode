d33da69c6da41a6e2e540d814af7a58d93382048
huanghao <11@.com>
2018-10-11 20:58:32 +0800

    文件服务器

--- http/file_server.js
+++ http/file_server.js
@@ -8,3 +8,10 @@ const
     /* 从命令行参数获取root目录，默认是当前目录 */
     const root = path.resolve(process.argv[2] || '.')
     console.log('Static root dir: ' + root);
+
+    /* 创建服务器 */
+
+    const server = http.createServer((request,response) => {
+        const pathName = url.parse(request.url).pathname
+        const filePath = path.join(root,pathName)
+    })
