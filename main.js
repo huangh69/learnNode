a60e7525153b1f5bd8234e2432ca2646fcb1e86d
huanghao <635184807@qq.com>
2018-10-15 22:59:45 +0800

    url处理

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 // require('./testFs.js')
 // require('./testStream.js')
 // require('./http/file_server.js')
-require('./KOA/app.js')
+require('./KOA/url2-koa/app.js')
 var greet = require('./hello/hello.js')
 
 var s = 'Huanghao'
